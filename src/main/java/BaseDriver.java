a9d2483329ccd62cbe2d1a234eb82ce742dce111
adriangonciarz <adrian.gonciarz@gmail.com>
2018-01-27 11:31:47 +0100

    quit driver

--- src/main/java/BaseDriver.java
+++ src/main/java/BaseDriver.java
@@ -1,40 +1,31 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServerHasNotBeenStartedLocallyException;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.URL;
+
 public class BaseDriver {
     public AppiumDriver<WebElement> driver;
-    private static AppiumDriverLocalService service;
 
     @Before
     public void setUp() throws Exception {
-        service = AppiumDriverLocalService.buildDefaultService();
-        service.start();
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
-        if (service == null || !service.isRunning()) {
-            throw new AppiumServerHasNotBeenStartedLocallyException(
-                    "An appium server node is not started!");
-        }
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName","nexus5");
+        capabilities.setCapability("deviceName", "nexus5");
         capabilities.setCapability("appPackage", "com.android.calculator2");
         capabilities.setCapability("appActivity", ".Calculator");
         capabilities.setCapability("platformVersion", "6.0");
-        driver = new AndroidDriver<>(service.getUrl(), capabilities);
+
+        driver = new AndroidDriver<>(url, capabilities);
+
     }
 
     @After
     public void tearDown() throws Exception {
-        if (driver != null) {
-            driver.quit();
-        }
-        if (service != null) {
-            service.stop();
-        }
+        driver.quit();
     }
 }
